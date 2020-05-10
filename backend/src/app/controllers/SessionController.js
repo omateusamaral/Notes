2fa58f055a02399720904b4d44a23a73c8f82935
mateusamarall <mateus.amaral018@gmail.com>
2020-05-09 23:16:57 -0300

    added Yup for Schema Validation

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,10 +1,18 @@
 const jwt = require('jsonwebtoken');
+const Yup = require('yup');
 const User = require('../models/User');
 const authConfig = require('../../config/auth');
 //controller de sessão
 module.exports = {
   //criando uma sessão
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } }); //pocurando um usuário pelo seu email
--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,9 +1,19 @@
+const Yup = require('yup');
 const User = require('../models/User');
 //controller de usuário
 module.exports = {
   //metodo para criar um usuario
   async store(req, res) {
-    const userExists = await UserModel.findOne({
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const userExists = await User.findOne({
       //verificando se um usuário já existe com email passado pelo usuario
       where: { email: req.body.email },
     });
@@ -12,7 +22,7 @@ module.exports = {
       return res.status(400).json({ error: 'User already exists' });
     }
     //se não existe é criado
-    const { id, name, email } = await UserModel.create(req.body);
+    const { id, name, email } = await User.create(req.body);
 
     return res.json({
       id,
@@ -22,19 +32,36 @@ module.exports = {
   },
   //metodo para atualizar um usuário
   async update(req, res) {
-    const { email, oldpassword } = req.body;
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      //quando o campo password é informado o confirmpassword tbm deve ser confirmado
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
 
     if (email !== user.email) {
       //se email digitado pelo usuário for diferente do que existe ele cai aqui
-      const userExists = await UserModel.findOne({ where: { email } });
+      const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
         return res.status(400).json({ error: 'User already exists' });
       }
     }
     //metodo para mudar a senha
-    if (oldpassword && !(await user.checkPassword(oldpassword))) {
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
     const { id, name } = await user.update(req.body);
