b32a12fa234f988dbcef2451bae911202e99f325
mateusamarall <mateus.amaral018@gmail.com>
2020-05-25 18:40:02 -0300

    changed the style of input

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import api from "../../services/api";
 import logo from "../../assets/logo.png";
 import { FiLogIn, FiArrowRight } from "react-icons/fi";
 import "./style.css";
-
+const initialValues = {
+  email: "",
+  password: "",
+};
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid E-mail").required("Campo obrigatório!"),
+  password: Yup.string().required("Campo obrigatório!").min("6"),
+});
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const history = useHistory();
 
-  async function handleLogin(event) {
-    event.preventDefault();
-
+  async function handleLogin(values) {
     try {
-      const response = await api.post("sessions", { email, password });
+      const response = await api.post("sessions", values);
 
       const { token } = response.data;
       if (token) {
         localStorage.setItem("token", `Bearer ${token}`);
 
         history.push("/dashboard");
-      } else {
-        alert("Dados não econtrado. Verifique o que foi digitado.");
       }
     } catch (error) {
       alert("Dados não econtrado. Verifique o que foi digitado.");
@@ -33,36 +36,43 @@ export default function Login() {
       <div className="login-container">
         <section className="form">
           <img src={logo} alt="Notes" />
-          <form onSubmit={handleLogin}>
-            <h1>
-              Bem vindo,
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleLogin}
+          >
+            <Form>
+              <h1>
+                Bem vindo,
+                <br />
+                <span> Faça seu login para continuar</span>
+              </h1>
+              <Field
+                type="email"
+                className="inputLogin"
+                placeholder="E-mail"
+                name="email"
+              />
+              <ErrorMessage name="email" />
               <br />
-              <span> Faça seu login para continuar</span>
-            </h1>
-            <input
-              type="email"
-              className="inputLogin"
-              placeholder="E-mail"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />{" "}
-            <br />
-            <input
-              type="password"
-              className="inputLogin"
-              placeholder="Senha"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-            <button type="submit" className="btnLogin">
-              Entrar <FiArrowRight size={20} color="#fff" />
-            </button>
-            <br />
-            <Link className="LinkRegister" to="/singup">
-              <FiLogIn size={16} color="#000" className="spaceFI" />
-              Realizar cadastro
-            </Link>
-          </form>
+              <Field
+                type="password"
+                className="inputLogin"
+                placeholder="Senha"
+                name="password"
+              />
+              <ErrorMessage name="password" />
+              <br />
+              <button type="submit" className="btnLogin">
+                Entrar <FiArrowRight size={20} color="#fff" />
+              </button>
+              <br />
+              <Link className="LinkRegister" to="/singup">
+                <FiLogIn size={16} color="#000" className="spaceFI" />
+                Realizar cadastro
+              </Link>
+            </Form>
+          </Formik>
         </section>
       </div>
 
