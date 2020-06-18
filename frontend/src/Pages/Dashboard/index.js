81fab97fff60b88e86dd3320135eb4fbe2859660
mateusamarall <mateus.amaral018@gmail.com>
2020-06-18 18:51:39 -0300

    added the function to delete a note, called the api to show the notes and solved the problem of date

--- frontend/src/Pages/Dashboard/index.js
+++ frontend/src/Pages/Dashboard/index.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { FiPower, FiX } from "react-icons/fi";
+import * as moment from "moment";
+import api from "../../services/api";
 import "./style.css";
 export default function Dashboard() {
-  const userName = localStorage.getItem("userName");
+  const [notes, setNotes] = useState([]);
   const history = useHistory();
+  const userName = localStorage.getItem("userName");
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    api
+      .get("/users/notes", {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        setNotes(res.data);
+      });
+  }, [token]);
+
+  async function handleDeleteNote(id) {
+    try {
+      await api.delete(`users/${id}/notes`, {
+        headers: {
+          Authorization: token,
+        },
+      });
+      setNotes(notes.filter((note) => note.id !== id));
+    } catch (error) {
+      alert("erro ao deletar note");
+    }
+  }
   function handleLogout() {
-    localStorage.clear();
+    localStorage.removeItem("userName");
     history.push("/");
   }
   return (
@@ -21,15 +49,17 @@ export default function Dashboard() {
       </header>
       <h1>Notes Cadastrados</h1>
       <ul>
-        <li>
-          <p>titulo</p>
-          <strong>descrição</strong>
-          <p>lembrete: 03/05/200 Horas: 01:10</p>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <p>{note.title}</p>
+            <strong>{note.description}</strong>
+            <p>lembrete: {moment(note.notify).format("DD/MM/YYYY-HH:mm")}</p>
 
-          <button>
-            <FiX size={20} color="#E02041" />
-          </button>
-        </li>
+            <button onClick={() => handleDeleteNote(note.id)}>
+              <FiX size={20} color="#E02041" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
