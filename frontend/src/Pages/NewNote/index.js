661d8766deaac886e07ce39999c37cd340c7b2d0
mateusamarall <mateus.amaral018@gmail.com>
2020-06-23 13:19:40 -0300

    added validations for form

--- frontend/src/Pages/NewNote/index.js
+++ frontend/src/Pages/NewNote/index.js
@@ -1,18 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as moment from "moment";
+import * as Yup from "yup";
+import { FiArrowLeft } from "react-icons/fi";
+import "./style.css";
 
+const initValues = {
+  title: "",
+  description: "",
+  date: moment().format("DD/MM/YYYY"),
+  time: moment().format("hh:mm"),
+};
+const validationSchema = Yup.object({
+  title: Yup.string().required("Campo obrigatório!"),
+  description: Yup.string().required("Campo obrigatório!"),
+  date: Yup.string().required("Campo obrigatório"),
+  time: Yup.string().required("campo obrigatório"),
+});
 export default function Newnote() {
   return (
     <div className="newnote-container">
       <header>
-        <button>Voltar</button>
-        <h3>Crie um novo note</h3>
+        <Link className="backlink" to="/dashboard">
+          <FiArrowLeft size={35} color="#000" />
+        </Link>
       </header>
-      <form>
-        <input type="text" name="title" placeholder="Título do note" />
-        <input type="text" name="decription" placeholder="Descrição  do note" />
-        <input type="date" name="notify" placeholder="data de lembrete" />
-        <button>Criar</button>
-      </form>
+      <Formik initialValues={initValues} validationSchema={validationSchema}>
+        <Form>
+          <h2>Crie um novo note</h2>
+          <Field type="text" name="title" placeholder="Título do note" />
+          <ErrorMessage name="title" />
+          <Field
+            type="text"
+            name="description"
+            placeholder="Descrição  do note"
+          />
+
+          <ErrorMessage name="description" />
+          <Field type="text" name="date" placeholder="data de lembrete" />
+          <ErrorMessage name="date" />
+
+          <Field type="time" name="time" placeholder="data de lembrete" />
+          <ErrorMessage name="time" />
+          <button>Criar</button>
+        </Form>
+      </Formik>
     </div>
   );
 }
