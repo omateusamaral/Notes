3745cb99a4af0868cb417826b6e51094b92c33ef
mateusamarall <mateus.amaral018@gmail.com>
2020-06-22 23:23:05 -0300

    starting the page to create a note

--- frontend/src/Pages/NewNote/index.js
+++ frontend/src/Pages/NewNote/index.js
@@ -2,8 +2,17 @@ import React from "react";
 
 export default function Newnote() {
   return (
-    <>
-      <h1>Criar Um note</h1>
-    </>
+    <div className="newnote-container">
+      <header>
+        <button>Voltar</button>
+        <h3>Crie um novo note</h3>
+      </header>
+      <form>
+        <input type="text" name="title" placeholder="Título do note" />
+        <input type="text" name="decription" placeholder="Descrição  do note" />
+        <input type="date" name="notify" placeholder="data de lembrete" />
+        <button>Criar</button>
+      </form>
+    </div>
   );
 }
