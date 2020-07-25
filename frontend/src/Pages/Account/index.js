8a46bba414e3b0a02f4de718da2b4a1ecf1b2f90
mateusamarall <mateus.amaral018@gmail.com>
2020-07-24 22:43:55 -0300

    page created

--- frontend/src/Pages/Account/index.js
+++ frontend/src/Pages/Account/index.js
@@ -1,7 +1,26 @@
 import React from "react";
-
-// import { Container } from './styles';
-
+import { Link } from "react-router-dom";
+import { FiChevronRight } from "react-icons/fi";
+import "./style.css";
 export default function Account() {
-  return <h1>olá</h1>;
+  return (
+    <div className="account-container">
+      <div className="edit-account">
+        <header>
+          <h1>Editar sua conta</h1>
+        </header>
+        <Link to="" className="edit-email">
+          <div>Email</div>
+          <span>
+            teste.com.br <FiChevronRight size={20} color="#000" />
+          </span>
+        </Link>
+        <Link to="" className="edit-password">
+          <div>
+            Senha <FiChevronRight size={20} color="#000" />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
