e118f907e71878d4475ec65e00ba02b5976d131d
mateusamarall <mateus.amaral018@gmail.com>
2020-07-25 00:04:59 -0300

    added the button to return a page

--- frontend/src/Pages/Account/index.js
+++ frontend/src/Pages/Account/index.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
 import { FiChevronRight } from "react-icons/fi";
 import "./style.css";
 export default function Account() {
+  const email = localStorage.getItem("email");
+
   return (
-    <div className="account-container">
-      <div className="edit-account">
-        <header>
-          <h1>Editar sua conta</h1>
-        </header>
-        <Link to="" className="edit-email">
-          <div>Email</div>
-          <span>
-            teste.com.br <FiChevronRight size={20} color="#000" />
-          </span>
-        </Link>
-        <Link to="" className="edit-password">
-          <div>
-            Senha <FiChevronRight size={20} color="#000" />
-          </div>
-        </Link>
+    <>
+      <Link className="backlink" to="/dashboard">
+        <FiArrowLeft size={35} color="#000" />
+      </Link>
+      <div className="account-container">
+        <div className="edit-account">
+          <header>
+            <h1>Editar sua conta</h1>
+          </header>
+          <Link to="/editemail" className="edit-email">
+            <div>Email</div>
+            <span>
+              {email} <FiChevronRight size={20} color="#000" />
+            </span>
+          </Link>
+          <Link to="" className="edit-password">
+            <div>
+              Senha <FiChevronRight size={20} color="#000" />
+            </div>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
